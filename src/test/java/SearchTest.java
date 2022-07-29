88a2979b51c2034a8ddfea02fa6fd8288ae6d44d
Ekaterina Zarubina <edubovenko@gmail.com>
2022-07-29 11:56:06 +0700

    search test remake

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -3,6 +3,7 @@ import jdk.jfr.Description;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import po.MainScreen;
+import po.SearchBottomSheet;
 
 
 import static org.junit.Assert.assertTrue;
@@ -14,16 +15,11 @@ public class SearchTest extends BaseTest {
             "when clicked search button on the main screen bottom panel")
     public void testOpenSearch() {
         MainScreen mainScreen = new MainScreen(driver);
+        SearchBottomSheet searchBottomSheet = new SearchBottomSheet(driver);
         mainScreen.clickSearchButton();
         mainScreen.clickSearchButton();
-
-
-
-        boolean isSearchSheetDisplayed = driver.findElement(By.xpath(".//android.view.View[@resource-id='search_categories_container']")).isDisplayed();
+        boolean isSearchSheetDisplayed = searchBottomSheet.isSearchCategoriesConteiner();
         assertTrue("Error: search category bottom sheet doesn't displays", isSearchSheetDisplayed);
-        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View[2]")).isDisplayed();
-        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View[2]")).click();
-
-        //driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[2]/android.view.View[1]/android.view.View[1]/android.widget.Button"));
+        searchBottomSheet.clickCloseButton();
     }
 }
